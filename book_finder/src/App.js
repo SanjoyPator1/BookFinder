022b498f51187514570b26f07df6be2013c84dfe
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-07-06 17:58:11 +0530

    Backend post request added for searching books

--- book_finder/src/App.js
+++ book_finder/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 const App = () => {
 
-  
+  const exampleReq = "http://127.0.0.1:5000/"
+
   return(
     <div className="App">
       <h1>Hello React</h1>
---
+++ flask/app1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 16:24:44 2021
+
+@author: asus
+"""
+
