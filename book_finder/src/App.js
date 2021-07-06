49ff4154aee84220732fbaf3a29374bed3300e41
SanjoyPator <sanjoy18-378@aec.ac.in>
2021-07-06 20:56:41 +0530

    search functionality added

--- book_finder/src/App.js
+++ book_finder/src/App.js
@@ -6,27 +6,37 @@ import Books from './books';
 const App = () => {
 
 
-  const [books, setBooks]=useState([])
-
-
+  const [books, setBooks]=useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('harry+potter+1')
 
   useEffect(() => {
     console.log("effect has been run");
     getBooks();
-  },[]);
+  },[query]);
 
   const getBooks = async () => {
-    const response = await fetch(`https://book-finder-backend.herokuapp.com/home/harry`);
+    const response = await fetch(`https://book-finder-backend.herokuapp.com/home/${query}`);
     const data = await response.json();
     console.log(data.books);
     setBooks(data.books)
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('')
+  }
+
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <form className="App">
-        <input className="search-bar" type="text" />
+      <form onSubmit={getSearch} className="App">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">submit</button>
       </form>
       {books.map(book => (
